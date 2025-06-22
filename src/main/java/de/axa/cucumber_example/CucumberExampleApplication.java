b372ad6c09d4f1aaf1387154b8f56de680a07107
Allan Mehlen <mehlen.allan@gmail.com>
2025-06-22 13:09:42 +0200

    feat: initial setup of spring boot project

---
+++ src/main/java/de/axa/cucumber_example/CucumberExampleApplication.java
@@ -0,0 +1,13 @@
+package de.axa.cucumber_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberExampleApplication.class, args);
+	}
+
+}
