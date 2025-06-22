e7a8995d0ac24cb84fa673b3f0432841be8b66ce
Allan Mehlen <mehlen.allan@gmail.com>
2025-06-22 15:10:54 +0200

    feat: add controllers for life insurance offers

---
+++ src/main/java/de/axa/cucumber_example/controller/ApplicationController.java
@@ -0,0 +1,41 @@
+package de.axa.cucumber_example.controller;
+
+import de.axa.cucumber_example.model.Application;
+import de.axa.cucumber_example.services.ApplicationService;
+import de.axa.cucumber_example.services.OfferService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+  private final OfferService offerService;
+  private final ApplicationService applicationService;
+
+  public ApplicationController(OfferService offerService, ApplicationService applicationService) {
+    this.offerService = offerService;
+    this.applicationService = applicationService;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Application> status(@PathVariable Long id) {
+    return applicationService.getState(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  public ResponseEntity<Application> neuerAntrag(@RequestParam Long applicationId,
+      @RequestParam String policyHolder) {
+    return offerService.findOffer(applicationId)
+        .map(application -> ResponseEntity.ok(applicationService.captureApplication(policyHolder, application)))
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+}
---
+++ src/main/java/de/axa/cucumber_example/controller/OfferController.java
@@ -0,0 +1,25 @@
+package de.axa.cucumber_example.controller;
+
+import de.axa.cucumber_example.model.Offer;
+import de.axa.cucumber_example.services.OfferService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/offer")
+public class OfferController {
+
+  private final OfferService offerService;
+
+  public OfferController(OfferService offerService) {
+    this.offerService = offerService;
+  }
+
+  @GetMapping
+  public Offer getOffer(@RequestParam String produkt, @RequestParam int laufzeit) {
+    return offerService.calculateAndSaveOffer(produkt, laufzeit);
+  }
+
+}
