b19002eaad320908b420825dfeeba25f77cfff83
Allan Mehlen <mehlen.allan@gmail.com>
2025-06-22 14:57:21 +0200

    fix: use correct argument in repository

--- src/main/java/de/axa/cucumber_example/repository/ApplicationRepository.java
+++ src/main/java/de/axa/cucumber_example/repository/ApplicationRepository.java
@@ -1,9 +1,10 @@
 package de.axa.cucumber_example.repository;
 
+import de.axa.cucumber_example.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<ApplicationRepository, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
 }
